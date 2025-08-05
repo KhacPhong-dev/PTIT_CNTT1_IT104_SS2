29dcdb630a5f600479530048dee199d54a18857c
KhacPhong-dev <kaito0676@gmail.com>
2025-08-05 23:05:56 +0700

    Add files via upload

--- formatData.js
+++ formatData.js
@@ -1,21 +1,14 @@
 const formatDate = (currentData) => {
-
     const today = new Date(currentData);
     let day = today.getDate();
-
-    if(day > 0 && day < 10){
+    if (day > 0 && day < 10) {
         day = `0${day}`;
     }
-
     let months = today.getMonth() + 1;
-
-    if(months > 0 && months < 10){
+    if (months > 0 && months < 10) {
         months = `0${months}`;
     }
-
     const year = today.getFullYear();
-
     return `${day}/ ${months} / ${year} `;
 }
-
 export default formatDate;
